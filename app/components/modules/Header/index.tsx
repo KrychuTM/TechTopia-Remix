3a865cf5b9ec2ee7efef4a9278bc8e8c1f631ff5
Jxavier Paul <jxavierpaul.dev@gmail.com>
2022-01-22 19:54:36 -0400

    Working on Product Card. Weird css styling for tall images.

--- app/components/modules/Header/index.tsx
+++ app/components/modules/Header/index.tsx
@@ -4,22 +4,30 @@ import { Link } from 'remix';
 
 function Header() {
   return (
-    <div className='flex flex-row items-center justify-between py-4 px-10 -mt-16 bg-[#EDEDED]'>
-      <button className='flex items-center justify-center p-2 bg-white rounded-md shadow-md'>
-        <MenuAlt4Icon className='w-7 h-7' />
-      </button>
-      <Link to="/">
+    <nav className='grid grid-cols-3 items-center py-4 px-4 md:px-10 -mt-16 bg-[#EDEDED] justify-items-stretch'>
+      <div className='flex flex-row items-center justify-self-start'>
+        <button className='flex items-center justify-center p-2 bg-white rounded-md shadow-md'>
+          <MenuAlt4Icon className='w-7 h-7' />
+        </button>
+        <div className='flex-row hidden mx-10 space-x-6 text-lg lg:flex'>
+          <p className='cursor-default link-underline'>New</p>
+          <p className='cursor-default link-underline'>Sales</p>
+          <p className='cursor-default link-underline'>Brands</p>
+          <p className='cursor-default link-underline'>Categories</p>
+        </div>
+      </div>
+      <Link to='/' className='justify-self-center'>
         <Logo extra='text-4xl' />
       </Link>
-      <div className='flex space-x-3 items-centers'>
+      <div className='flex space-x-3 justify-self-end'>
         <button className='flex items-center justify-center p-2 bg-white rounded-md shadow-md'>
           <ShoppingBagIcon className='w-7 h-7' />
         </button>
-        <button className='flex items-center justify-center p-2 bg-white rounded-md shadow-md'>
+        <button className='items-center justify-center hidden p-2 bg-white rounded-md shadow-md md:flex'>
           <UserIcon className='w-7 h-7' />
         </button>
       </div>
-    </div>
+    </nav>
   );
 }
 
--- app/routes/__main/index.tsx
+++ app/routes/__main/index.tsx
@@ -1,11 +1,39 @@
 const Index = () => {
   return (
-    <div className="px-10 py-4">
-      <h1 className='flex text-4xl font-semibold text-[#292A2F] font-bebas'>
-        Welcome To TechTopia. $299
-      </h1>
+    <div className='px-4 py-4 md:px-10'>
+      <section>
+        <div title='section-header' className='mb-3'>
+          <h3 className='flex text-xl md:text-3xl font-semibold text-[#292A2F]'>
+            Featured This Month
+          </h3>
+        </div>
+        <div
+          title='section-content'
+          className='grid grid-cols-2 gap-3 md:gap-0 md:grid-cols-3 lg:grid-cols-4'
+        >
+          <div
+            title='product-card'
+            className='px-[3vw] py-[4vw] md:p-0 w-max md:w-max bg-white border-2 border-transparent rounded-lg hover:border-indigo-600 md:hover:border-transparent md:bg-transparent'
+          >
+            <div
+              title='product-img'
+              className='flex items-center justify-center bg-transparent w-[36vw] md:w-[260px] md:h-80 md:bg-white border-2 border-transparent rounded-lg md:hover:border-indigo-600 hover:border-transparent'
+            >
+              <img src='https://media.direct.playstation.com/is/image/sierialto/dualsense-ps5-controller-black-accessory-front?$Background_Large$' />
+            </div>
+            <h4 title='product-brand' className='mt-5 text-xs sm:text-sm md:text-sm'>
+              Sony
+            </h4>
+            <h5 title='product-name' className='text-xs'>
+              Playstation 5 Controller
+            </h5>
+            <span>$350.00</span>
+          </div>
+          <div>Hi Again</div>
+        </div>
+      </section>
     </div>
   );
-}
- 
+};
+
 export default Index;
